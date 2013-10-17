6ba1a1dd37e4f378637b536770bc7a80c027a31d
Umair Ashraf <umair@scrapinghub.com>
2013-10-17 17:29:19 +0500

    added wsgi code for nginx server

--- api/server.py
+++ api/server.py
@@ -99,5 +99,6 @@ class Profile(JSONFeed):
         return json.dumps([])
 
 if __name__ == '__main__':
+    web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
     app = web.application(urls, globals())
     app.run()
