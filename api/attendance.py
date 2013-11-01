6dcb0628b4e74465a43967e4ce89c2bd130d28e0
Umair Ashraf <umr.ashrf@gmail.com>
2013-11-02 02:03:51 +0500

    fixed presents and absensts in attendance

--- api/attendance.py
+++ api/attendance.py
@@ -23,9 +23,9 @@ def get_attendance(session):
         semester = '%s %s' % (get_season(today), today.strftime('%Y'))
         course_name = get_first(tr.xpath('td[contains(@class, "attendanceRowCourse")]/text()')).strip()
         course_id = get_first(re.findall('\((\d+)\)$', course_name))
-        sessions = get_first(tr.xpath('td[2 and contains(@class, "attendanceRowStat")]/text()')).strip()
-        presents = get_first(tr.xpath('td[3 and contains(@class, "attendanceRowStat")]/text()')).strip()
-        absents = get_first(tr.xpath('td[4 and contains(@class, "attendanceRowStat")]/text()')).strip()
+        sessions = get_first(tr.xpath('td[2]/text()')).strip()
+        presents = get_first(tr.xpath('td[3]/text()')).strip()
+        absents = get_first(tr.xpath('td[4]/text()')).strip()
         attendance = [semester, course_id, course_name, sessions, presents, absents]
         attendances.append(dict(zip(fields, attendance)))
     return attendances
