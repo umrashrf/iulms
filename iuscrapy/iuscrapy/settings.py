324937e742790d3a2668feb319704393982b9492
Umair Ashraf <umr.ashrf@gmail.com>
2013-12-28 17:47:36 +0500

    added image pipeline and settings

--- iuscrapy/iuscrapy/settings.py
+++ iuscrapy/iuscrapy/settings.py
@@ -4,3 +4,9 @@ SPIDER_MODULES = ['iuscrapy.spiders']
 NEWSPIDER_MODULE = 'iuscrapy.spiders'
 
 User_Agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.65 Safari/537.36'
+
+ITEM_PIPELINES = {
+    'scrapy.contrib.pipeline.images.ImagesPipeline': 1
+}
+
+IMAGES_STORE = '/home/umair/Development/IQRAUniversity/src/iuscrapy/images'
